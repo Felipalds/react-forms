def71ec984c42d95593bf714f6cf5df4715cd77e
luizfelipefrosa <luizfelipefonsecarosa@gmail.com>
2021-11-20 12:21:39 -0300

    Adding form CEP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import axios from 'axios'
 
 function App() {
 
   const [ form, setForm ] = React.useState({
+    name: '',
     email: '',
-    password: ''
+    password: '',
+    cep: '',
+    street: ''
   })
+
+  const [ resp, setResp ] = React.useState('')
 
   const handleChange = ({target}) => {
     const { id, value } = target
@@ -18,15 +23,51 @@ function App() {
     console.log(form)
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault()
+    try{
+    await axios.post('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(form)
+    })
+    setResp('Sucess')
+    }catch(err){
+      setResp('Err')
+    }
   }
+
+  const handleCep = async({target}) => {
+    const { id, value } = target
+    setForm({
+      ...form,
+      [id]:value
+    })
+    if(form.cep.length === 8)
+    try{
+      fetch(`https://viacep.com.br/ws/${form.cep}/json`).then(res => res.json()).then(data => console.log(data))
+    } catch(err){
+      return 0
+    }
+  }
+
+
 
   return (
     <main>
       <h1>Formulários com React</h1>
       {form.email ? <h2>{form.email}</h2> : null}
       <form onSubmit={handleSubmit}>
+        <input
+          id="name"
+          name="name"
+          placeholder="Nome completo"
+          type="text"
+          value={form.name}
+          onChange={handleChange}>
+        </input>
         <input 
           id="email"
           name="email"
@@ -35,7 +76,6 @@ function App() {
           value={form.email}
           onChange={handleChange}>
         </input>
-        <br /><br />
         <input
           id="password"
           name="email"
@@ -45,10 +85,17 @@ function App() {
           onChange={handleChange}
           >
         </input>
+        <input 
+          id="cep"
+          name="cep"
+          placeholder="CEP"
+          type="number"
+          value={form.cep}
+          onChange={handleCep}>
+        </input>
 
-
-        <br/><br/>
         <button type="submit">Login</button>
+        {resp ? <div id="r">{resp}</div> : null}
       </form>
     </main>
   );
